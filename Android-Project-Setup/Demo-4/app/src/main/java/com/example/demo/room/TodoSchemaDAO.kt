782232ce66ba5d7cbecc20d759ed1bed1dca326e
RohalSaini <rohalsainia94@gmail.com>
2021-09-20 10:14:12 +0530

    dem0 4

---
+++ Android-Project-Setup/Demo-4/app/src/main/java/com/example/demo/room/TodoSchemaDAO.kt
@@ -0,0 +1,21 @@
+package com.example.demo.room
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TodoSchemaDAO {
+
+    @Insert
+    suspend fun addTodo(todo: TodoSchema)
+
+    @Query("SELECT * FROM todo order by id ")
+    fun readAllData():LiveData<List<TodoSchema>>
+
+//    @Query("SELECT * FROM todo WHERE email LIKE :email")
+//    fun getTodoByEmail(email:String): LiveData<List<TodoSchema>>
+}
