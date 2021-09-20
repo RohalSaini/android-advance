4fd1406c485b22efa2d1895a46e9ebcd882eef68
RohalSaini <rohalsainia94@gmail.com>
2021-09-20 10:55:42 +0530

    demo 5

---
+++ Android-Project-Setup/Demo-5/app/src/main/java/com/example/demo/room/TodoSchemaRepository.kt
@@ -0,0 +1,16 @@
+package com.example.demo.room
+
+import androidx.lifecycle.LiveData
+import kotlinx.coroutines.flow.Flow
+
+class TodoSchemaRepository(private val todoSchemaDAO: TodoSchemaDAO) {
+    val readAllData:LiveData<List<TodoSchema>> = todoSchemaDAO.readAllData()
+
+    suspend fun addTodo(todo: TodoSchema) {
+        todoSchemaDAO.addTodo(todo = todo)
+    }
+
+//    fun findTodoByEmail(email:String): LiveData<List<TodoSchema>> {
+//        return todoSchemaDAO.getTodoByEmail(email=email)
+//    }
+}
